35697b03c7c1bf487b1eb48adc412d9659c75d80
VilhelmTheBrave <willschau1991@gmail.com>
2020-12-15 01:10:00 -0500

    Update support_functions.py
    Added function comments

--- support_functions.py
+++ support_functions.py
@@ -11,6 +11,8 @@ class INTERACTIVE_GEN_OPTIONS(Enum):
   Continue = 2
   Quit     = 3
 
+# Roll a dice, defaults to d6
+#--------------------------------------------------#
 def roll_dice(numRolls=1,diceMin=1,diceMax=6):
   curRollCount = 0
   result = 0
@@ -18,7 +20,10 @@ def roll_dice(numRolls=1,diceMin=1,diceMax=6):
     result += randint(diceMin, diceMax)
     curRollCount += 1
   return result
+#--------------------------------------------------#
 
+# Gets an entry from a table
+#--------------------------------------------------#
 def get_table_entry(header, table, index):
   selectedEntry = table[index]
   headerLength = len(header)
@@ -28,7 +33,10 @@ def get_table_entry(header, table, index):
     entryString += header[i] + ": " + table[index][i] + "\n"
     i += 1
   return entryString
+#--------------------------------------------------#
 
+# Combines a list of strings into a single string
+#--------------------------------------------------#
 def convert_slist_to_string(listOfStrings):
   stringListLen = len(listOfStrings)
   retString = ""
@@ -37,24 +45,36 @@ def convert_slist_to_string(listOfStrings):
     retString += entry
   retString = "None" if len(retString) == 0 else retString
   return retString
+#--------------------------------------------------#
 
+# Clears the terminal
+#--------------------------------------------------#
 def clear_screen():
   sysName = system()
   if sysName == 'Windows':
     call("cls", shell=True)
   elif sysName == 'Linux':
     call("clear", shell=True)
+#--------------------------------------------------#
 
+# Prints the list of script options
+#--------------------------------------------------#
 def print_option_list(optionList):
   for option in optionList:
     print(str(option.value) + " = " + option.name)
+#--------------------------------------------------#
 
+# Gets the list of valid script option values
+#--------------------------------------------------#
 def get_option_values(optionList):
   valueList = []
   for option in optionList:
     valueList.append(option.value)
   return valueList
+#--------------------------------------------------#
 
+# Prompts the user to save an asset
+#--------------------------------------------------#
 def save_output_dialog(scriptDir, outputData, outputTypeName):
   try:
     if len(outputData) > 0:
@@ -78,10 +98,16 @@ def save_output_dialog(scriptDir, outputData, outputTypeName):
   except KeyboardInterrupt:
     clear_screen()
     print(outputData + "Failed to save " + outputTypeName + "\n")
+#--------------------------------------------------#
 
+# Gets the path to the script directory
+#--------------------------------------------------#
 def get_cur_dir_path(fileSelfRef):
   return path.dirname(path.realpath(fileSelfRef))
+#--------------------------------------------------#
 
+# Prompts the user to choose a script option
+#--------------------------------------------------#
 def user_input_dialog(genOptions, extraDisplayString):
   currentOption = 0
   valid_option_values = get_option_values(genOptions)
@@ -92,7 +118,10 @@ def user_input_dialog(genOptions, extraDisplayString):
     currentOptionInput = input ("\nChoose a valid option: ")
     currentOption = int(currentOptionInput) if currentOptionInput.isdigit() else 0
   return currentOption
+#--------------------------------------------------#
 
+# Prompts the user to do interactive generation
+#--------------------------------------------------#
 def do_interactive_gen_loop(interactiveGenOption, loopFunc, funcArgs):
   while True:
     retString, retValue = loopFunc(funcArgs)
@@ -104,4 +133,4 @@ def do_interactive_gen_loop(interactiveGenOption, loopFunc, funcArgs):
     elif interactiveGenOption == INTERACTIVE_GEN_OPTIONS.Quit.value:
       break
   return interactiveGenOption, retString, retValue
-  
+#--------------------------------------------------#
